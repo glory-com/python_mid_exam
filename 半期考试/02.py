8bc20c8287bbff199918fde75adbd60b8f668a60
ChenWu <1740662936@qq.com>
2025-03-18 12:41:09 +0800

    练习题1-题目B

--- 半期考试/02.py
+++ 半期考试/02.py
@@ -2,8 +2,6 @@
 # 中位数是指将N个数字按照大小排列起来居于数据列表中间位置的数值。当N为偶数时，取中间两个数的平均值， 当N为奇数时，取中间那个数
 # 实现list_median函数，输入为含有N个数字的列表，返回它的中位数，如果列表为空则返回None
 
-
-
 def list_median(input_list):
     #如果列表为空，返回None
     if len(input_list) == 0:
